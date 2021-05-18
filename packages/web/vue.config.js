a49dc45ae0bea51b996a3fbc5b4709f292fbe672
Pyxis <pyxis@uzuki.live>
2021-05-19 04:27:10 +0900

    add build output setting for web

--- packages/web/vue.config.js
+++ packages/web/vue.config.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 module.exports = {
   transpileDependencies: ["vuetify"],
+  outputDir: path.resolve(__dirname, "../../dist/web"),
 
   pluginOptions: {
     i18n: {
